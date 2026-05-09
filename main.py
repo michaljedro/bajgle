660779c9a7e333b53472fb408a1dd8532c5aa788
Michał Jędro <michal.jedro@wp.pl>
2026-05-09 18:37:03 +0200

    all done

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import random
 
-
-
-secret_number = '123'
+def generate_secret_number():
+     secret_number = str(random.randint(100,999))
+     if isinstance(secret_number, str):
+          print(secret_number)
+     return secret_number
 def main():
     print("LET'S PLAY THE GAME")
+    secret_number = generate_secret_number()
     while True:      
         user_num = input()
         clues = get_clues(user_num, secret_number)
